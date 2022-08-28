6fa266bc15e595853542ff6c2936d3ebf3cfd929
desraymondz <110300950+desraymondz@users.noreply.github.com>
2022-08-28 15:03:34 +0700

    added super idol and js css pages

---
+++ page.js
@@ -0,0 +1,15 @@
+const today = new Date();
+document.querySelector('#current-date').innerHTML = today.toDateString();
+document.querySelector('#button-style').addEventListener('click', function() {
+// document.getElementById("memes").innerHTML = "wow naughty kiddo🤨"
+document.getElementById("memes").innerHTML = "BOO!"
+document.getElementById("memes").style.fontSize = '15vw'
+const audio = new Audio('Super-Idol.mp3');
+audio.play();
+});
+// document.querySelector('#button-style').addEventListener('mouseover', function() {
+//   document.getElementById("memes").innerHTML = "HAHAHA"
+// });
+// document.querySelector('#button-style').addEventListener('mouseout', function() {
+//   document.getElementById("memes").innerHTML = "NOPE"
+// });
