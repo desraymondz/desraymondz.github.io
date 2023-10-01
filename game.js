91ba5992a59a042611e7e122e1533cb8868d65b4
desraymondz <110300950+desraymondz@users.noreply.github.com>
2023-10-02 02:00:37 +0700

    added game.html and js

---
+++ game.js
@@ -0,0 +1,66 @@
+function questionsGenerator () {
+    const operators = ['+','-','*','/']
+    const randomOperatorsPicker = Math.floor(Math.random() * 4)
+    const chosenOperator = operators[randomOperatorsPicker]
+
+    let variable1 = randomNumber(0,100)
+    const variable2 = randomNumber(0,100)
+
+    let answer 
+    switch (chosenOperator) {
+        case '+':
+            answer = variable1 + variable2
+            break;
+        case '-':
+            answer = variable1 - variable2
+            break;
+        case '*':
+            answer = variable1 * variable2
+            break;
+        case '/':
+            answer = variable1
+            variable1 = variable1 * variable2
+            break;
+    }
+    return [variable1, variable2 , answer, chosenOperator]
+
+}
+
+function randomNumber (min,max) {
+    let randomNumber = Math.random()
+    randomNumber = randomNumber * (max - min + 1) + min
+    randomNumber = Math.floor(randomNumber)
+
+    return randomNumber
+}
+
+let correctOperator
+
+document.querySelector('#generateQ').addEventListener('click', function() {
+    const q = questionsGenerator()
+    correctOperator = q[3]
+    
+    document.querySelector('#variable1').innerHTML = q[0]
+    document.querySelector('#operator').innerHTML = "?"
+    document.querySelector('#variable2').innerHTML = q[1]
+    document.querySelector('#answer').innerHTML = q[2]
+    document.querySelector('#questionText').style.backgroundColor= 'transparent'
+    
+})
+
+const operatorButtons = document.querySelectorAll('.operatorsButton')
+
+operatorButtons.forEach(function(el){
+    el.addEventListener('click', function() {
+
+        document.querySelector('#operator').innerHTML = el.dataset.operator
+        
+        if (el.dataset.operator == correctOperator) {
+            document.querySelector('#questionText').style.backgroundColor= '#3f3'
+        } else {
+            document.querySelector('#questionText').style.backgroundColor= '#f33'
+            
+        }
+    })
+})
+
--- page.js
+++ page.js
@@ -2,11 +2,11 @@ const today = new Date();
 document.querySelector('#current-date').innerHTML = today.toDateString();
 document.querySelector('#button-style').addEventListener('click', function() {
 // document.getElementById("memes").innerHTML = "wow naughty kiddo🤨"
-document.getElementById("memes").innerHTML = "BOO!"
-document.getElementById("memes").style.fontSize = '15vw'
-document.querySelector('#super-gif').style.display = 'inline';
-const audio = new Audio('Super-Idol.mp3');
-audio.play();
+    document.getElementById("memes").innerHTML = "BOO!"
+    document.getElementById("memes").style.fontSize = '15vw'
+    document.querySelector('#super-gif').style.display = 'inline';
+    const audio = new Audio('Super-Idol.mp3');
+    audio.play();
 });
 // document.querySelector('#button-style').addEventListener('mouseover', function() {
 //   document.getElementById("memes").innerHTML = "HAHAHA"
@@ -14,3 +14,6 @@ audio.play();
 // document.querySelector('#button-style').addEventListener('mouseout', function() {
 //   document.getElementById("memes").innerHTML = "NOPE"
 // });
+
+
+
